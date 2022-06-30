3613551871d9eadd7160d2a085b82e758a39a1ee
Yasemin Gürbüz <84576138+yasemingurbuz@users.noreply.github.com>
2022-06-30 09:55:47 +0300

    Add files via upload

--- TestiniumRestAsured/src/test/java/trello.java
+++ TestiniumRestAsured/src/test/java/trello.java
@@ -1,13 +1,9 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-
-
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 
-
-import java.util.ArrayList;
 
 import static io.restassured.RestAssured.given;
 import static org.testng.AssertJUnit.assertEquals;
@@ -16,35 +12,35 @@ public class trello {
     String key = "28cb5a571f71e9eeadb531e0b9969ad7";
     String token = "04312436e1726025671852722f742ebe32e996da17fe2ad949188b0f7310d2dd";
     String URL = "https://api.trello.com/";
+    String boardName = "ProjectBoardName";
+    String cardName = "CardName";
+    String updateName = "ProjectUpdatedName";
+    String idList = "62b9e09c749ac538c59e987e";
+    String idBoards = "62b9dfae60286a0ea3863ff1";
+    String deleteBoard = "62b9e5f96fdf278bed8e88b5";
 
-    String boardname;
-    String cardname;
-    String updatename;
-    String idBoards;
-    String idList;
-    String idcard;
 
-
-    ArrayList<String> cardID_list = new ArrayList<>();
 
     @Test
-     public void CreateBoard(){
+     public void CreateBoard() throws InterruptedException {
 
          RestAssured.baseURI = URL;
          System.out.println(URL);
          Response response = (Response) given()
                  .contentType(ContentType.JSON)
-                 .log().all()
+                 .log()
+                 .all()
                  .queryParam("key", key)
                  .queryParam("token",token)
-                 .queryParam("name",boardname)
-                 .when().post("/1/boards/");
-        System.out.println(response);
-        idBoards = response.body().jsonPath().getString("id");
-        assertEquals(response.statusCode(), 200);
+                 .queryParam("name",boardName)
+                 .when()
+                 .post("/1/boards/")
+                 .then()
+                 .assertThat()
+                 .statusCode(200)
+                 .extract()
+                 .response();
         assertEquals(response.contentType(), "application/json; charset=utf-8");
-
-
     }
 
     @Test
@@ -53,31 +49,38 @@ public class trello {
 
         for (int i = 0; i < 2; i++) {
             RestAssured.baseURI = URL;
-
             response = (Response) given().log().all()
                     .contentType(ContentType.JSON)
-                    .queryParam("name", cardname)
+                    .queryParam("name", cardName)
                     .queryParam("key", key)
                     .queryParam("token", token)
-                    .queryParam("idBoard",idBoards)
-                    .post("/cards/");
-
-            idList = response.body().jsonPath().getString("id");
-
+                    .queryParam("idList",idList)
+                    .post("/1/cards/")
+                    .then()
+                    .assertThat()
+                    .statusCode(200)
+                    .extract()
+                    .response();
         }
     }
+
     @Test
     public void updateBoard(){
         RestAssured.baseURI = URL;
         System.out.println(URL);
+        String request = "/1/boards/" + idBoards;
         Response response = (Response) given()
                 .contentType(ContentType.JSON)
                 .log().all()
                 .queryParam("key", key)
                 .queryParam("token",token)
-                .queryParam("name",updatename)
-                .when().put("/cards/id{id}");
-
+                .queryParam("name",updateName)
+                .put(request)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .response();
     }
 
     @Test
@@ -87,10 +90,10 @@ public class trello {
         System.out.println(URL);
         Response response = (Response) given().log().all()
                 .contentType(ContentType.JSON)
-                .queryParam("name",boardname)
+                .queryParam("name",boardName)
                 .queryParam("key", key)
                 .queryParam("token",token)
-                .delete("https://api.trello.com/1/boards/" + idBoards);
+                .delete("https://api.trello.com/1/boards/" + deleteBoard);
 
     }
 
